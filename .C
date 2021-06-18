ecf8a2ce783b301b67348b90699eacb31b2bba7f
Amrit kumar <79562154+kramrit95@users.noreply.github.com>
2021-06-18 19:05:34 +0530

    Create .C

---
+++ .C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include<math.h>
+int main() {
+    int houses;
+    scanf("%d", &houses);
+
+    //your code goes here
+    printf ("%f",round((2*100)/houses));
+    
+    return 0;
+}
